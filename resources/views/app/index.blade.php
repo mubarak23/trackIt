e05108123f36b3888df6056d71b0a76316f38b10
mubarak23 <mubarakaminu340@gmail.com>
2021-04-27 16:30:03 +0100

    citizen dashboard UI

---
+++ resources/views/app/index.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+@section('content')
+    <div class="flex space-x-4 p-4 justify-center">
+        <div class="w-3/12  bg-white p-6">
+            <section class="mb-3">
+            <p class="font-bold">Recent Project</p>
+            <div class="bg-gray-300 fakeimg" style="height: 200px">Image</div>
+            <p class="font-medium">SUPPLY OF KEKE NAPEP AT KUDAN/MAKARFI FEDERAL CONSTITUENCY OF KADUNA STATE, Kaduna state</p>
+            <p class="font-light">3 month ago</p>
+            </section>
+            <section>
+                <p><p class="font-bold">Recent Reported Project</p></p>
+            <div class="mb-2">
+                 <p class="font-medium">Medical Awareness Campaign/Medical Mission/Supplies Of
+                    Equipment In Kudan/Makarfi L.G.A Of Kaduna State., Kaduna state</p>
+            <p class="font-light">3 month ago</p>
+            </div>
+            <div class="mb-2">
+                <p class="font-medium">Youth Development Center at Tohu, Sabon Gari Federal Constituency Kaduna., Kaduna state</p>
+            <p class="font-light">3 month ago</p>
+            </div>
+
+            <div class="mb-2">
+                <p class="font-medium">SUPPLY OF AMBULANCE IN MAKARFI/KUDAN FED. CONST, Kaduna state</p>
+            <p class="font-light">3 month ago</p>
+            </div>
+
+            <div class="mb-2">
+                <p class="font-medium">Supply of Motorcycles at Jaba/zango Kataf Federal Constituency Kaduna State, Kaduna state</p>
+            <p class="font-light">3 month ago</p>
+            </div>
+            </section>
+        </div>
+         <div class="w-9/12 bg-white p-6 rounded-lg">
+            <p class="font-bold">Dashboard | Top Reported Project</p>
+            <section>
+        <div class="main">
+        <h2>TITLE HEADING</h2>
+        <h5>Title description, Jan 7, 2021</h5>
+        <div class=" bg-gray-300 fakeimg" style="height: 200px">Image</div>
+        <p>Some text..</p>
+        <p>
+          Sunt in culpa qui officia deserunt mollit anim id est laborum
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco.
+        </p>
+        <br />
+        <h2>TITLE HEADING</h2>
+        <h5>Title description, Spe 18, 2020</h5>
+        <div class="bg-gray-300 fakeimg" style="height: 200px">Image</div>
+        <p>Some text..</p>
+        <p>
+          Sunt in culpa qui officia deserunt mollit anim id est laborum
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco.
+        </p>
+      </div>
+        </section>
+    </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <link>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+        <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+        <title>TrackIt</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}" />
+        <!-- Styles -->
+        <style>
+
+        </style>
+    </head>
+    <title>Track It</title>
+    <body class="bg-gray-50">
+
+        <nav class="navbar navbar-expand-md navbar-dark mb-4" style="background-color:#3097D1">
+
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#responsive"><span class="navbar-toggler-icon"></span></button>
+
+
+        <div class="collapse navbar-collapse" id="responsive">
+            <ul class="navbar-nav mr-auto text-capitalize">
+                <li class="nav-item"><a href="index.html" class="nav-link active">home</a></li>
+                <li class="nav-item"><a href="profile.html" class="nav-link">Project</a></li>
+                <li class="nav-item"><a href="#modalview" class="nav-link" data-toggle="modal">Communnity</a></li>
+                <li class="nav-item"><a href="notification.html" class="nav-link">Report</a></li>
+                <li class="nav-item"><a href="#" class="nav-link ">logout</a></li>
+            </ul>
+
+            <form action="" class="form-inline ml-auto d-none d-md-block">
+                <input type="text" name="search" id="search" placeholder="Search" class="form-control form-control-sm">
+            </form>
+            <a href="notification.html" class="text-decoration-none" style="color:#CBE4F2;font-size:22px;"><i class="far fa-bell ml-3 d-none d-md-block"></i></a>
+        </div>
+    </nav>
+
+        @yield('content')
+    </body>
+    <div class=" footer">
+      <h2 class="justify-center">Footer</h2>
+    </div>
+</html>
